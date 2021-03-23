aed4e6c5b8144109c196cd7c83f8bc7999b6dac2
imran nazir <inazir2016@gmail.com>
2021-03-22 22:47:02 -0400

    js

--- js/main.js
+++ js/main.js
@@ -8,7 +8,31 @@ if (JSON.parse(localStorage.getItem('dark-theme-enabled'))) {
 }
 
 /* select option */
-function myFunction() {
-  const project = document.getElementById("mySelect").value;
-  document.getElementById("select").innerHTML = "You selected: " + project;
-}
+
+  let project = document.getElementById("mySelect")
+  //document.getElementById("select").innerHTML = "You selected: " + project;
+  project.addEventListener('change', ()=>{
+    console.log(project.value)
+    if(project.value !== "all-project"){
+  document.getElementById("select").textContent = 'You selected: '}
+ else{
+  document.getElementById("select").textContent = ''
+ }
+  document.querySelectorAll(".app").forEach(app =>{
+    console.log("app")
+    console.log(app)
+    if(project.value === 'all-project'){
+   app.classList.remove("invisible")
+   app.classList.remove("visible")
+    } else if(project.value === app.classList[1]){
+      console.log("we are here")
+      app.classList.remove("invisible")
+      app.classList.add("visible")
+    
+    }else{
+      app.classList.remove("visible")
+      app.classList.add("invisible")
+    }
+  })
+  
+  })
